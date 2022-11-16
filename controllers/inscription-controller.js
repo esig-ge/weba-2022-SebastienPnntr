8ddd7ff3ac11124102e53eb8784bba041481e16e
Seebii11 <36041410+1Sebi@users.noreply.github.com>
2022-11-16 22:16:45 +0100

    Ajout du model User

--- controllers/inscription-controller.js
+++ controllers/inscription-controller.js
@@ -1,7 +1,11 @@
+const {user} = require("../models/user");
+
+let monUser = new user(1, "Lenon", "Jhon", "Jhon@lenon.com", "0796096058");
+
 // Affichage de la page inscription.ejs avec les données définies en paramètres
 const registerView = (req, res) => {
     res.render("inscription", {
-        test: "c'est un test"
+        user: monUser,
     });
 }
 
---
+++ models/user.js
@@ -0,0 +1,13 @@
+let user = class User{
+    constructor(id, nom, prenom, email, telephone){
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+    }
+}
+
+module.exports = {
+    user,
+}
