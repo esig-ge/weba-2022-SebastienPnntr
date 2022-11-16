0dd522d07bb50e5027369792078885aba6c43f20
1Sebi <sebastien.pnntr@eduge.ch>
2022-11-16 15:34:09 +0100

    Ajout de la DB Sqlite3

--- controllers/inscription-controller.js
+++ controllers/inscription-controller.js
@@ -1,4 +1,4 @@
-// Pour la page d'inscription
+// Affichage de la page inscription.ejs avec les données définies en paramètres
 const registerView = (req, res) => {
     res.render("inscription", {
         test: "c'est un test"
