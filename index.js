33f03932a4ec88b408e9e585a38f90bdee45553a
1Sebi <sebastien.pnntr@eduge.ch>
2022-11-16 13:39:02 +0100

    Frontend page de connexion

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 3000
 // Routes
 app.use('/', require('./routes/routeur'));
 
+// Pour les ressources publiques
+app.use(express.static(__dirname + '/public'));
+
 // Ecoute sur le port
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- routes/routeur.js
+++ routes/routeur.js
@@ -1,7 +1,17 @@
 const express = require('express');
+
+// Import controllers
 const { loginView } = require('../controllers/connexion-controller.js');
 const { registerView } = require('../controllers/inscription-controller.js')
+
+// Router
 const router = express.Router();
 router.get('/inscription', registerView);
 router.get('/connexion', loginView);
+
+// Default root
+router.get('/', (req, res) => {
+    res.redirect("inscription");
+})
+
 module.exports = router;
