63b0864ad757a30fc4c7f9846c98cb11846b44d0
1Sebi <sebastien.pnntr@eduge.ch>
2022-11-18 14:41:34 +0100

    Mise au propre pour commencer la connexion a la DB

--- controllers/inscription-controller.js
+++ controllers/inscription-controller.js
@@ -1,19 +1,14 @@
-const {user} = require("../models/user");
-const {body, validationResult} = require("express-validator");
-
-let monUser = new user(1, "Lenon", "Jhon", "Jhon@lenon.com", "0796096058");
-
 // Affichage de la page inscription.ejs avec les données définies en paramètres
 const registerView = (req, res) => {
     res.render("inscription", {
-        user: monUser,
+        user: "",
     });
 }
 
 const getRegisterForm = (req, res) => {
     console.log("test"); // Quand il recoit le form
     res.render("inscription", {
-        user: monUser,
+        user: "",
     });
 }
 
