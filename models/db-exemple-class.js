24c62098c69464dc863ed231cf5f606e4da294ed
Seebii11 <36041410+1Sebi@users.noreply.github.com>
2022-11-18 01:40:17 +0100

    clarification detail exemple db

--- models/db-exemple-class.js
+++ models/db-exemple-class.js
@@ -8,6 +8,7 @@ class dbExemple{
         // Ici mettre l'appelle a la db pour le getallusers, dans le controller, initialiser la classe dbExemple et appeller cette methode pour get all users
         // genre: db = new dbExemple()
         // db.getAllUsers
+        // NE PAS OUBLIER DE RETURN L'OBJET USER ET NON PAS JUSTE LES DONNEES (un tableau d'objet user en l'occurence)
     }
 
     getUserById(id){
