1a2c54619ae34a18a8db86a26b2f32daef322f4a
Seebii11 <36041410+1Sebi@users.noreply.github.com>
2022-11-23 18:37:08 +0100

    Base de la page connexion

--- controllers/connexion-controller.js
+++ controllers/connexion-controller.js
@@ -1,9 +1,22 @@
-// Pour la connexion
-const loginView = (req, res) => {
+const { User } = require('../models/user');
+const sequelize = require('../models/database');
+const bcrypt = require('bcryptjs');
+
+// Affiche la page connexion.ejs avec les données définies en paramètres
+const loginView = async(req, res) => {
 
     res.render("connexion", {
     });
 }
+
+const getLoginForm = async(req, res) => {
+    try{
+
+    }catch{
+
+    }
+}
+
 module.exports = {
     loginView
 };
