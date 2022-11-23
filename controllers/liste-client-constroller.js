1c16b9a8c04e8f2287db639df5135435428ade4a
Seebii11 <36041410+1Sebi@users.noreply.github.com>
2022-11-23 19:36:21 +0100

    Ajout de express-session

--- controllers/liste-client-constroller.js
+++ controllers/liste-client-constroller.js
@@ -1,5 +1,6 @@
 const { User } = require('../models/user');
 const { sequelize } = require('../models/database');
+const session = require('express-session')
 
 // Affiche la page connexion.ejs avec les données définies en paramètres
 const listeClientView = async(req, res) => {
