de9ac09c0f0a8b56ed7dc080bc7480ec9ec066d7
Seebii11 <36041410+1Sebi@users.noreply.github.com>
2022-11-23 18:26:42 +0100

    Cryptage du mot de passe + Double verification du mot de passe

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - [Sequelize (Pour l'ORM)](https://sequelize.org/)
 - [SQLite](https://sqlite.org)
 - [EJS (Pour mes vues)](https://ejs.co/)
+- [Bcrypt](https://www.npmjs.com/package/bcryptjs)
 ## Sources 
 - [MVC avec Express.js](https://blog.logrocket.com/building-structuring-node-js-mvc-application/)
 - [Documentation de Sequelize](https://sequelize.org/docs/v6/getting-started/)
@@ -12,3 +13,4 @@
 - [Documentation d'Express](https://expressjs.com/en/starter/installing.html)
 - [Style des messages d'erreurs](https://codepen.io/roydigerhund/pen/grgKbj)
 - [Style formulaire](https://codepen.io/Basel-a-k/pen/bGMxpQN)
+- [Video pour comprendre Bcrypt](https://www.youtube.com/watch?v=_XxrfGrdrB8)
--- controllers/inscription-controller.js
+++ controllers/inscription-controller.js
@@ -1,5 +1,6 @@
 const { User } = require('../models/user');
 const { sequelize } = require('../models/database');
+const bcrypt = require('bcryptjs');
 
 // Affichage de la page inscription.ejs avec les données définies en paramètres
 const registerView = async(req, res) => {
@@ -19,10 +20,21 @@ const getRegisterForm = async(req, res) => {
             res.render("inscription", {
                 error: "L'email est déjà utilisé par un autre compte!"
             })
+        } else if(req.body.mdp != req.body.mdpConfirm){
+                // Si l'utilisateur n'a pas bien confirmé son mot de passe
+            res.render("inscription", {
+                error: "Les mots de passe ne correspondent pas"
+            })
         }
         else{
+            // Cryptage du mot de passe
+            // Generation du grain de sel
+            let salt = await bcrypt.genSalt(10)
+            // Cryptage du mot de passe
+            let hash = await bcrypt.hash(req.body.mdp, salt);
+
             // Créer un nouvel utilisateur si l'email n'est pas déjà utilisé et renvois sur la page
-            let user = await User.create({nomUser: req.body.nom, prenomUser: req.body.prenom, emailUser: req.body.email, telUser: req.body.tel, mdpUser: req.body.mdp, adminUser: false })
+            let user = await User.create({nomUser: req.body.nom, prenomUser: req.body.prenom, emailUser: req.body.email, telUser: req.body.tel, mdpUser: hash, adminUser: false })
             res.render("connexion", {
                 newUser: true
             })
