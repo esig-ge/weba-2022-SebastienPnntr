7a926a5fc574e3096dd5090d9f8f89c932b4288b
1Sebi <sebastien.pnntr@eduge.ch>
2022-12-16 14:48:01 +0100

    Passage de mon script dans un fichier.js

---
+++ public/js/liste-client.js
@@ -0,0 +1,34 @@
+const emails = document.getElementsByClassName("user-li");
+const searchBar = document.getElementById("searchBar");
+const listUser = document.getElementById("listUser");
+
+searchBar.addEventListener("input", searchUser);
+
+async function searchUser() {
+    let searchParam = await searchBar.value.toUpperCase();
+    let nbHide = 0;
+    Array.from(emails).forEach(element => {
+        if (element.textContent.toUpperCase().includes(searchParam)) {
+            element.classList.remove('d-none');
+        }
+        else {
+            element.classList.add('d-none');
+            nbHide += 1;
+        }
+    });
+
+    let liError = document.getElementById('msgError');
+    if (nbHide == emails.length) {
+        if (!liError) {
+            let msgErreur = document.createElement("li");
+            msgErreur.setAttribute('id', 'msgError')
+            msgErreur.textContent = "Aucun utilisateur trouvé";
+            listUser.append(msgErreur);
+        }
+    }
+    else {
+        if (liError) {
+            liError.remove();
+        }
+    }
+}
